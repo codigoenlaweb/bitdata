688c35afa542a83476aad6d70a56d97fd9b5edfd
codigoenlaweb <codeenlaweb@gmail.com>
2021-11-10 20:05:18 -0400

    database logic performed

---
+++ app/Http/Controllers/ComentsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Coments;
+use Illuminate\Http\Request;
+
+class ComentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Coments  $coments
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Coments $coments)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Coments  $coments
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Coments $coments)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Coments  $coments
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Coments $coments)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Coments  $coments
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Coments $coments)
+    {
+        //
+    }
+}
